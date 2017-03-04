9bad94bad2ca9545b71e999ee7fc7ad4722091b9
Yuri Brito <yuriabrito@gmail.com>
2017-03-04 01:49:55 -0300

    bad solution to non threadsafe uv_write

--- write.cpp
+++ write.cpp
@@ -1,7 +1,6 @@
 #include <uv.h>
 #include <uv_msg_framing.h>
 #include <mutex>
-#include <iostream>
 #include "write.h"
 
 namespace film { namespace network {
@@ -15,15 +14,19 @@ void write(Message message) {
   memcpy(buf->base, message.data, buf->len);
 
   uv_msg_write_t* msg_req = new uv_msg_write_t();
-  msg_req->data = (void*) buf;
+  struct BufBool { char* buf; bool* done; };
+  auto done = new bool(false);
+  auto buf_bool = new BufBool { .buf = buf->base, .done = done };
+  msg_req->data = (void*) buf_bool;
 
   std::lock_guard<std::mutex> lock(mutex);
   uv_msg_send(msg_req, message.handle, buf->base, buf->len,
     [](uv_write_t* req, int status) -> void {
-      auto helper = (uv_buf_t*) req->data;
-      if(req->data) delete (uv_buf_t*) req->data;
-      if(req) delete req;
+    auto buf_bool = static_cast<BufBool*>(req->data);
+    *(buf_bool->done) = true;
+    delete buf_bool->buf;
   });
+  while(!done);
 }
 
 } }
